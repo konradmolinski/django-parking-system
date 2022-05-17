62d6fb9c76c1122bccb77a788d3eadf67325312f
Konrad <konradsebastianmolinski@gmail.com>
2022-05-17 14:42:16 +0200

    initial commit

---
+++ parking/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Client(models.Model):
+    plate_num = models.CharField(unique=True, max_length=50)
+    voucher = models.DurationField()
+
+class ParkingEntry(models.Model):
+
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    billing_type = models.CharField(max_length=50)
+    client = models.ForeignKey(Client, null=True, on_delete=models.SET_NULL)
+    plate_num = models.CharField(max_length=50)
+    overtime = models.DurationField()
+    reservation_id = models.ForeignKey('Reservation', null=True, on_delete=models.SET_NULL)
+
+class PaymentRegister(models.Model):
+
+    payment_date = models.DateTimeField(auto_now_add=True)
+    parking_entry_id = models.ForeignKey(ParkingEntry, on_delete=models.PROTECT)
+    amount = models.IntegerField()
+
+class Reservation(models.Model):
+    client_id = models.ForeignKey(Client, null=True, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+    payment_id = models.ForeignKey(PaymentRegister, on_delete=models.CASCADE)
+
+class Subscription(models.Model):
+
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+    payment_id = models.ForeignKey(PaymentRegister, on_delete=models.CASCADE)
+    parking_spot = models.IntegerField()
+
+
