a5dbd8537d16fec89da4572beca6f3a42606b920
Konrad <konradsebastianmolinski@gmail.com>
2022-05-19 00:02:27 +0200

    parking spot url fix

--- parking/urls.py
+++ parking/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import parkingAPIView
 
 urlpatterns = [
-    path('', parkingAPIView.as_view({'get': 'retrieve'})),
+    path('free-spots', parkingAPIView.as_view({'get': 'retrieve'})),
 ]
