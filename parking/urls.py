b63e46f1ef513f775cfff4c037a2b6218827e05f
Konrad <konradsebastianmolinski@gmail.com>
2022-05-20 18:12:17 +0200

    Basic frontend for counting parking spots feature

--- parking/urls.py
+++ parking/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import parkingAPIView
+from .views import parkingAPIView, home
 
 urlpatterns = [
     path('free-spots', parkingAPIView.as_view({'get': 'retrieve'})),
+    path('', home, name='home'),
 ]
--- parking/views.py
+++ parking/views.py
@@ -1,9 +1,17 @@
 from rest_framework import generics, viewsets
 from rest_framework.response import Response, Response
 from .models import ParkingEntry
+from django.shortcuts import render
+import requests
 import json
 
 class parkingAPIView(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         free_spots = ParkingEntry.free_spots(ParkingEntry)
         return Response({"free_spots": free_spots})
+
+def home(request):
+    res = requests.get('http://127.0.0.1:8000/api/free-spots').json()
+    return render(request, 'parking/home.html', {
+        'free_spots': res['free_spots']
+    })
