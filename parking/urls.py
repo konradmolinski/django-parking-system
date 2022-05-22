d06ad0e48963508429b54dfbf3384894ddadefcc
Konrad <konradsebastianmolinski@gmail.com>
2022-05-22 15:53:06 +0200

    Updated settings.py with HOSTNAME var

--- parking/urls.py
+++ parking/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import parkingAPIView, parkingSpotsView
+from .views import ParkingAPIView, ParkingSpotsView
 
 urlpatterns = [
-    path('api/free-spots', parkingAPIView.as_view({'get': 'retrieve'})),
-    path('web/free-spots', parkingSpotsView.as_view()),
+    path('api/free-spots', ParkingAPIView.as_view({'get': 'retrieve'})),
+    path('web/free-spots', ParkingSpotsView.as_view()),
 ]
--- parking/views.py
+++ parking/views.py
@@ -1,15 +1,16 @@
 from django.views.generic import TemplateView
-from rest_framework import generics, viewsets
-from rest_framework.response import Response, Response
+from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import ParkingEntry
+from django.conf import settings
 
 
-
-
-class parkingAPIView(viewsets.ViewSet):
+class ParkingAPIView(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         free_spots = ParkingEntry.free_spots(ParkingEntry)
         return Response({"free_spots": free_spots})
 
-class parkingSpotsView(TemplateView):
+
+class ParkingSpotsView(TemplateView):
     template_name = "free_spots.html"
+    extra_context = {"hostname": settings.HOSTNAME}
