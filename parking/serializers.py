582f69ff71f1f943ad3d5e95930b81fc4a95ca91
Konrad <konradsebastianmolinski@gmail.com>
2022-05-25 14:12:07 +0200

    Added insert possibility to ParkingEntry table on frontend mock ticket machine event

--- parking/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-
-class UserStorySerializer(serializers.Serializer):
-    plate_num = serializers.CharField(max_length=50)
--- parking/views.py
+++ parking/views.py
@@ -1,9 +1,7 @@
 from django.views.generic import TemplateView
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .models import ParkingEntry
-from .serializers import UserStorySerializer
+from .models import ParkingEntry, Client
 from django.conf import settings
 
 
@@ -16,13 +14,10 @@ class ScreenAPIView(viewsets.ViewSet):
 class UserStoryAPIView(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         plate_num = request.data
+        if ParkingEntry.free_spots(ParkingEntry):
+            entry = ParkingEntry(billing_type="ad hoc", plate_num=plate_num["Plate Number"])
+            entry.save()
         return Response(plate_num)
-# @api_view(['POST'])
-# def UserStoryAPIView(request):
-#     serializer = UserStorySerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#     return Response(serializer.data)
 
 class ParkingSpotsView(TemplateView):
     template_name = "free_spots.html"
